14fd8279fd964b9b8e4abb9e610dc8f7b89512fc
forever97 <857426255@qq.com>
2021-01-29 17:12:50 +0800

    update

--- src/Comment.js
+++ src/Comment.js
@@ -1,6 +1,35 @@
 import React, { Component } from 'react'
 
 class Comment extends Component {
+    constructor () {
+        super()
+        this.state = { timeString: '' }
+    }
+    
+    UNSAFE_componentWillMount () {
+        this._updateTimeString()
+        this._timer = setInterval(
+            this._updateTimeString.bind(this),
+            5000
+        )
+    }
+
+    _updateTimeString () {
+        const comment = this.props.comment
+        const duration = (+Date.now() - comment.createdTime) / 1000
+        this.setState({
+            timeString: duration > 60
+            ? `${Math.round(duration / 60)} 分钟前`
+            : `${Math.round(Math.max(duration, 1))} 秒前`
+        })
+    }
+
+    handleDeleteComment () {
+        if (this.props.onDeleteComment) {
+            this.props.onDeleteComment(this.props.index)
+        }
+    }
+
     render () {
         return (
             <div className='comment'>
@@ -8,8 +37,20 @@ class Comment extends Component {
                     <span>{this.props.comment.username} </span>：
                 </div>
                 <p>{this.props.comment.content}</p>
+                <span className='comment-createdtime'>
+                    {this.state.timeString}
+                </span>
+                <span
+                    onClick={this.handleDeleteComment.bind(this)}
+                    className='comment-delete'>
+                    删除
+                </span>
             </div>
         )
+    }
+
+    componentWillUnmount () {
+        clearInterval(this._timer)
     }
 }
 
--- src/CommentInput.js
+++ src/CommentInput.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 class CommentInput extends Component {
+
     constructor () {
         super()
         this.state = {
@@ -20,11 +21,24 @@ class CommentInput extends Component {
     }
     handleSubmit () {
         if (this.props.onSubmit) {
-            const { username, content } = this.state
-            this.props.onSubmit({username, content})
+            // const { username, content } = this.state
+            this.props.onSubmit({
+                username: this.state.username,
+                content: this.state.content,
+                createdTime: +new Date()
+            })
         }
         this.setState({ content: '' })
     }
+    
+    _saveUsername (username) {
+        localStorage.setItem('username', username)
+    }
+
+    handleUsernameBlur (event) {
+        this._saveUsername(event.target.value)
+    }
+
     render() {
         return (
             <div className='comment-input'>
@@ -33,6 +47,7 @@ class CommentInput extends Component {
                     <div className='comment-field-input'>
                         <input
                             value={this.state.username}
+                            onBlur={this.handleUsernameBlur.bind(this)}
                             onChange={this.handleUsernameChange.bind(this)} />
                     </div>
                 </div>
@@ -40,6 +55,7 @@ class CommentInput extends Component {
                     <span className='comment-field-name'>评论内容：</span>
                     <div className='comment-field-input'>
                         <textarea 
+                            ref={(textarea) => this.textarea = textarea}
                             value={this.state.content}
                             onChange={this.handleContentChange.bind(this)} />
                     </div>
@@ -51,6 +67,22 @@ class CommentInput extends Component {
                 </div>
             </div>
         )
+    }
+
+    componentDidMount () {
+        this.textarea.focus()
+    }
+    
+    
+    _loadUsername () {
+        const username = localStorage.getItem('username')
+        if (username) {
+            this.setState({ username })
+        }
+    }
+
+    UNSAFE_componentWillMount () {
+        this._loadUsername()
     }
 }
 
